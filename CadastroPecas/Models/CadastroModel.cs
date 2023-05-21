0dd79901c32f3c60298a45ad72f2b631a2cbd5be
Augusto Monteiro <augustoengenharia@outlook.com>
2023-05-21 17:01:49 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroPecas/Models/CadastroModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CadastroPecas.Models
+{
+    [Table("Produto")]
+    public class CadastroModel
+    {
+        [Column("id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("Marca")]
+        [Display(Name = "Marca")]
+        public string Marca { get; set; }
+
+        [Column("Modelo")]
+        [Display(Name = "Modelo")]
+        public string Modelo { get; set; }
+
+        [Column("Peça")]
+        [Display(Name = "Peça")]
+        public string Peca { get; set; }
+    }
+}
---
+++ CadastroPecas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CadastroPecas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
